7ae953570341573535150b164acb51b84c7f208e
zigakk03 <zigakk03@gmail.com>
2023-05-15 11:08:30 +0200

    Database connection and added all the entities

---
+++ src/entities/quote.entity.ts
@@ -0,0 +1,24 @@
+import { Expose } from 'class-transformer';
+import { IsUUID } from 'class-validator';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+
+
+@Entity()
+export class Quote {
+  @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
+  @Expose()
+  quote_id: string;
+
+  @CreateDateColumn()
+  @Expose()
+  created_at: Date
+
+  @Column({ nullable: false })
+  text: string
+
+  @ManyToOne(() => User, {onDelete: 'SET NULL'})
+  @JoinColumn({ name: 'user_id' })
+  user: User | null
+}
